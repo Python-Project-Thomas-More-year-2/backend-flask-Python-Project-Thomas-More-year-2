83862415b53d94857cb206b8008761e2573812e1
Lander Van laer <lander.vanlaer1@gmail.com>
2022-12-01 13:33:50 +0100

    Add socket-id to user in DB

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from datetime import timedelta
+from typing import Optional
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request, session
+from flask.sessions import SessionMixin
 from flask_cors import CORS
 from flask_restful import Api
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 from jsonschema import ValidationError
 
-from models import db
+from models import db, User
 from routes.HelloWorld import HelloWorld
 from routes.SessionJoinRoute import SessionJoinRoute
 from routes.SessionRoute import SessionRoute
 
 app = Flask(__name__)
 api = Api(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", manage_session=False)
 
 CORS(app, resource={"*": {"origins": "*"}}, supports_credentials=True)
 
@@ -41,9 +43,46 @@ def bad_request(error):
     return error
 
 
+def get_user_by_session(ses: SessionMixin) -> Optional[User]:
+    # Get user-id from session
+    user_id = ses.get("user_id")
+
+    # Has session connected
+    if user_id is None:
+        return None
+
+    # Get user from database
+    user: User = User.query.filter_by(id=user_id).first()
+
+    # Check if user exists
+    if user is None:
+        # Session remove 'connected' user
+        session.pop("user_id")
+        return None
+
+    return user
+
+
 @socketio.on('connect')
-def test_connect():
-    emit('connect', {'data': 'Connected'})
+def connect():
+    user = get_user_by_session(session)
+
+    # Deny connect when no session user active
+    if user is None:
+        return False
+
+    # Add socket-id to user
+    user.socketSessionId = request.sid
+    db.session.commit()
+
+
+@socketio.on('disconnect')
+def disconnect():
+    user = get_user_by_session(session)
+
+    if user is not None:
+        user.socketSessionId = None
+        db.session.commit()
 
 
 if __name__ == "__main__":
