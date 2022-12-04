66de3a2e6c86eac15abeb959d3e3ef826dbce00d
Lander Van laer <lander.vanlaer1@gmail.com>
2022-12-04 15:32:56 +0100

    Add user auth feature

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from datetime import timedelta
 
-from flask import Flask, make_response, jsonify, request, session
+from flask import Flask, make_response, jsonify, session, request
 from flask_cors import CORS
 from flask_restful import Api
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from jsonschema import ValidationError
 
 from helpers.get_user_by_session import get_user_by_session
-from models import db
+from models import db, User
 from routes.HelloWorld import HelloWorld
 from routes.SessionJoinRoute import SessionJoinRoute
 from routes.SessionPlayerList import SessionPlayerList
@@ -48,15 +48,43 @@ def bad_request(error):
 
 @socketio.on('connect')
 def connect():
-    user = get_user_by_session(session)
+    # No implementation
+    pass
 
-    # Deny connect when no session user active
+
+@socketio.on("auth")
+def auth(json):
+    if "socketConnection" not in json:
+        emit("auth-res", {
+            "error": "invalid data send"
+        })
+        return
+
+    socket_connection = json.get("socketConnection")
+    # find user with the given code
+    user = User.query.filter_by(socketConnection=socket_connection).first()
+
     if user is None:
-        return False
+        emit("auth-res", {
+            "error": "no user found with the given socket_connection"
+        })
+        return
+
+    # user exists
+
+    # delete other connection if exist
+    user_prev_connection: User | None = User.query.filter_by(socketSessionId=request.sid).first()
+    if user_prev_connection is not None:
+        user_prev_connection.socketSessionId = None
+        db.session.commit()
 
     # Add socket-id to user
     user.socketSessionId = request.sid
     db.session.commit()
+
+    emit("auth-res", {
+        "message": "connected to session"
+    })
 
 
 @socketio.on('disconnect')
