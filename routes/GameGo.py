6e75151f69146b8db4d3f9864713300f1b760455
YiikerGiiker <101648320+YiikerGiiker@users.noreply.github.com>
2022-12-20 16:24:54 +0100

    gamego

--- routes/GameGo.py
+++ routes/GameGo.py
@@ -1,9 +1,9 @@
-from flask import session
+from flask import request, session
 from flask_expects_json import expects_json
 from flask_restful import Resource
 
 from helpers.get_user_by_session import get_user_by_session
-from models import Session
+from models import Session, User, db
 
 schema_post = {
     "type": "object",
@@ -13,9 +13,6 @@ schema_post = {
             "properties": {
                 "id": {
                     "type": "integer"
-                },
-                "money": {
-                    "type": "integer"
                 }
             },
             "required": ["id"],
@@ -24,33 +21,24 @@ schema_post = {
     "required": ["user"],
 }
 
-class GameStart(Resource):
+class GameGo(Resource):
     @staticmethod
     @expects_json(schema_post)
     def post():
+        req = request.get_json()
+
         user = get_user_by_session(session, throw_unauthorized=True)
 
         user.assert_is_host()
 
-        if user is None:
-            return {}, 404
+        u = User.query.filter_by(id=req["user"]["id"], session_id=user.session_id).first()
 
-        user.money += Session.goReward
+        u.money += u.session.goReward
+        db.session.commit()
 
-        if Session.seeOthersBalance:
-            user.emit_to_session()
+        if u.session.seeOthersBalance:
+            user.emit_to_session('money change', u.money)
         else:
-            user.emit()
+            user.emit('money change', u.money)
 
-        user.update({
-            "money": user.money
-        })
-        
         return {}, 200
-
-        
-
-        
-
-
-        
