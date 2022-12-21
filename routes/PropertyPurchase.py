3499a69d76af017a7156fa837b2e7d3ea25ecbb7
Lander Van laer <lander.vanlaer1@gmail.com>
2022-12-21 21:44:46 +0100

    Fix 500 error

--- routes/PropertyPurchase.py
+++ routes/PropertyPurchase.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import Conflict, NotFound
 from helpers.get_user_by_session import get_user_by_session
 from models import User, db, Transaction
 
-#Add schema
+# Add schema
 schema_post = {
     "type": "object",
     "properties": {
@@ -48,6 +48,7 @@ schema_put = {
     "required": ["transaction"],
 }
 
+
 class PropertyPurchase(Resource):
     @staticmethod
     @expects_json(schema_post)
@@ -56,36 +57,35 @@ class PropertyPurchase(Resource):
 
         user = get_user_by_session(session, throw_unauthorized=True)
 
-        #assert_is_host
+        # assert_is_host
         user.assert_is_host()
 
-        #Check if user is logged in
+        # Check if user is logged in
         u = User.query.filter_by(id=req["user"]["id"], session_id=user.session_id).first()
 
         if u is None:
             raise NotFound("User does not exist")
 
-        #check if session has started
+        # check if session has started
         if not u.session.started:
             raise Conflict("Session has not started yet")
 
-        #create transaction and store in database, transaction.request_sender = None (bank)
+        # create transaction and store in database, transaction.request_sender = None (bank)
         t = Transaction(
-            request_sender_id = None,
-            request_payer_id = req["user"]["id"],
-            amount = req["transaction"]["amount"]
+            request_sender_id=None,
+            request_payer_id=req["user"]["id"],
+            amount=req["transaction"]["amount"]
         )
         db.session.add(t)
         db.session.commit()
 
-        #send transaction-object to transaction.request_payer
+        # send transaction-object to transaction.request_payer
         u.emit('transaction-requested', {
-                "transaction":t.to_object()
-            })
+            "transaction": t.to_object()
+        })
 
-        #return no content
+        # return no content
         return {}, 204
-
 
     @staticmethod
     @expects_json(schema_put)
@@ -94,22 +94,23 @@ class PropertyPurchase(Resource):
 
         u = get_user_by_session(session, throw_unauthorized=True)
 
-        #Check if the user is the transaction.request_payer of the given transaction.id
+        # Check if the user is the transaction.request_payer of the given transaction.id
         t = Transaction.query.filter_by(id=req["transaction"]["id"], request_payer_id=u.id).first()
 
         if t is None:
             raise NotFound("transaction not found")
 
-
-        #Check if user has enough money
+        # Check if user has enough money
         if u.money < t.amount:
             raise Conflict("User does not have enough money to afford this transaction")
 
         u.money -= t.amount
+        db.session.delete(t)
         db.session.commit()
+
         u.emit_balance_update()
 
-       # Respond
+        # Respond
         return {
             "user": {
                 "id": u.id,
