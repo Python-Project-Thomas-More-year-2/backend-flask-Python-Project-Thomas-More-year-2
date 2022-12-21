347ebde8ae9da1755adf8d8a4ecab78ea47c5ec7
Matthew Wuyts <wuytsma@gmail.com>
2022-12-21 22:17:05 +0100

    updated

--- routes/SessionStartRoute.py
+++ routes/SessionStartRoute.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 from werkzeug.exceptions import Conflict
 
 from helpers.get_user_by_session import get_user_by_session
-from models import db
+from models import db, User
 
 
 class SessionStartRoute(Resource):
@@ -17,6 +17,11 @@ class SessionStartRoute(Resource):
             raise Conflict("Can not start session twice")
 
         user.session.started = True
+
+        u: User
+        for u in user.session.users:
+            u.money = user.session.startCapital
+
         db.session.commit()
 
         user.emit_to_session("session-start", {})
